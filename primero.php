496ec17eb9707b2b07c4220d77eeef9c49994c56
osbert <1310591@utp.edu.pe>
2020-08-12 10:47:27 -0500

    segundo commit

---
+++ primero.php
@@ -0,0 +1,14 @@
+
+<html lang="en-PE">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="author" content="Edward Santamaria Benites" />
+    <meta name="description" content="mensajes cristianos" />
+    <meta name="keywords" content="mensaje,cristiano,biblia,frases" />
+    <link rel="icon" href="ed.png" type="image/x-icon"><!--64x64 imagen de la pagina en la pestaña-->
+    <title>mensajes cristianos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@1,500&display=swap" rel="stylesheet">
